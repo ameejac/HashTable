8c0db9259afe55d57cb561b15cef204d5278242a
ameejac <ameejacherukuru123@gmail.com>
2021-10-06 09:44:25 -0700

    [added: MyMapcode ]

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //calling a MyMapNode class.
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(6);
+            hash.Add("1", "To");
+            hash.Add("2", "be");
+            hash.Add("3", "or");
+            hash.Add("4", "not");
+            hash.Add("5", "to");
+            hash.Add("6", "be");
+            string hash6 = hash.Get("6");
+            Console.WriteLine("6th index value:" + hash6);
+            string hash5 = hash.Get("5");
+            Console.WriteLine("5th index value:" + hash5);
+            string hash4 = hash.Get("4");
+            Console.WriteLine("4th index value:" + hash4);
+            string hash3 = hash.Get("3");
+            Console.WriteLine("3ed index value:" + hash3);
+            string hash2 = hash.Get("2");
+            Console.WriteLine("2nd index value:" + hash2);
+            string hash1 = hash.Get("1");
+            Console.WriteLine("1st index value:" + hash1);
+            //Calling Count Frequency Method.
+
+            hash.CountFrequency("To be or not to be");
+            Console.ReadLine();
+        }
+    }
+}
